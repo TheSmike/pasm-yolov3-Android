ad4b6826901a921d8853c1fd9fd4dc7352a89236
smike86 <mike.smike86@gmail.com>
2019-04-08 18:31:37 +0200

    Initial commit from local

---
+++ app/src/main/java/it/unipr/scarpentim/pasmtftest1/MainActivity.java
@@ -0,0 +1,314 @@
+package it.unipr.scarpentim.pasmtftest1;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+import java.util.List;
+
+import it.unipr.scarpentim.pasmtftest1.tensorflow.Classifier;
+import it.unipr.scarpentim.pasmtftest1.tensorflow.TensorFlowImageClassifier;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
+
+    private boolean disable = false;
+    private Classifier classifier = null;
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    Mat mRgba;
+    Mat mRgbaF;
+    Mat mRgbaT;
+
+    private static final String TAG = "PASM-yolov3";
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
+    private Bitmap myBitmap = null;
+
+    private Menu mOptionsMenu;
+
+
+    private static final int INPUT_WIDTH = 300;
+    private static final int INPUT_HEIGHT = 300;
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128f;
+    private static final String INPUT_NAME = "Mul";
+    private static final String OUTPUT_NAME = "final_result";
+    private static final String MODEL_FILE = "file:///android_asset/hero_stripped_graph.pb";
+    private static final String LABEL_FILE = "file:///android_asset/hero_labels.txt";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+        validateCameraPermission();
+        initClassifier();
+        mOpenCvCameraView= findViewById(R.id.cameraView1);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+//        mOpenCvCameraView.setMaxFrameSize(1280, 720);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        //Log.i(TAG, "mOpenCvCameraView size (w,h):" + mOpenCvCameraView.getWidth() + mOpenCvCameraView.getHeight());
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        mOptionsMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_openGallery) {
+            validateReadStoragePermission();
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_PICK);
+            startActivityForResult(Intent.createChooser(intent,"Seleziona immagine"), SELECT_PICTURE);
+            return true;
+        }else if (id == R.id.action_openCamera) {
+            ImageView iv = findViewById(R.id.ivGallery);
+            iv.setVisibility(View.GONE);
+            myBitmap = null;
+            mOpenCvCameraView.enableView();
+            mOptionsMenu.getItem(1).setEnabled(false);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    Bitmap smallBitmap = null;
+
+    public void classify(View view) {
+
+        Mat mRgbaTemp = mRgba.clone();
+        //mOpenCvCameraView.disableView();
+        //mOpenCvCameraView.setVisibility(View.GONE);
+        new ComputeTask().execute(mRgbaTemp);
+        TextView tv = findViewById(R.id.textView);
+        tv.setText("I'm thinking...");
+    }
+
+    //region - Metodi da CvCameraViewListener2
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(width, height, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, height, CvType.CV_8UC4);
+        Log.i(TAG, "height : " + height);
+        Log.i(TAG, "width : " + width);
+        //Log.i(TAG, "mOpenCvCameraView size (w,h):" + mOpenCvCameraView.getWidth() +  " - " + mOpenCvCameraView.getHeight());
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+        mRgbaF.release();
+        mRgbaT.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return inputFrame.rgba();
+    }
+    //endregion
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Chiama l'inizializzazione asincrona e passa l'oggetto callback
+        // creato in precendeza, e sceglie quale versione di OpenCV caricare.
+        // Serve anche a verificare che l'OpenCV manager installato supporti
+        // la versione che si sta provando a caricare.
+        if (!disable)
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION,this, mLoaderCallback);
+    }
+
+    private class ComputeTask extends AsyncTask<Mat, Bitmap, List<Classifier.Recognition>>{
+
+        @Override
+        protected List<Classifier.Recognition> doInBackground(Mat... mats) {
+            Mat mRgbaTemp = mats[0];
+            if (myBitmap != null){
+                smallBitmap = Bitmap.createScaledBitmap(myBitmap, INPUT_WIDTH, INPUT_HEIGHT, false);
+            }else{
+
+                smallBitmap = Bitmap.createBitmap(INPUT_WIDTH, INPUT_HEIGHT, Bitmap.Config.RGB_565);
+                Bitmap bigBitmap = Bitmap.createBitmap(mRgbaF.width(), mRgbaF.height(), Bitmap.Config.RGB_565);
+                Mat mRgbaFixedSize = new Mat(INPUT_WIDTH, INPUT_HEIGHT, CvType.CV_8UC4);
+
+                Core.transpose(mRgbaTemp, mRgbaT);
+                Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+                Core.flip(mRgbaF, mRgbaTemp, 1 );
+
+                Imgproc.resize(mRgbaTemp, mRgbaFixedSize, new Size(INPUT_WIDTH, INPUT_HEIGHT), 0,0, 0);
+
+                Utils.matToBitmap(mRgbaFixedSize, smallBitmap);
+                Utils.matToBitmap(mRgbaTemp, bigBitmap);
+
+                this.publishProgress(bigBitmap);
+
+
+                //OLD Toast.makeText(getApplicationContext(), "Nessuna immagine caricata", Toast.LENGTH_SHORT).show();
+            }
+
+            List<Classifier.Recognition> recognitions = classifier.recognizeImage(smallBitmap);
+            return  recognitions;
+        }
+
+        @Override
+        protected void onPostExecute(List<Classifier.Recognition> result) {
+            TextView tv = findViewById(R.id.textView);
+            Log.i(TAG, String.valueOf(result));
+            for (Classifier.Recognition recognition : result) {
+                Log.i(TAG, "results: " + recognition.getTitle() + " " + recognition.getConfidence());
+            }
+            if (result.size() > 0)
+                tv.setText("This is.... " + result.get(0).getTitle());
+            else
+                tv.setText("This is.... Not a super hero");
+
+            ImageView iv = findViewById(R.id.imageView1);
+            iv.setImageBitmap(smallBitmap);
+//            mOpenCvCameraView.enableView();
+//            mOpenCvCameraView.setVisibility(View.VISIBLE);
+
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Bitmap... values) {
+            Log.i(TAG, "### onProgressUpdate called!!");
+            super.onProgressUpdate(values[0]);
+            ImageView iv = findViewById(R.id.imageView1);
+            iv.setImageBitmap(values[0]);
+            iv.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void validateReadStoragePermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission is not granted");
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_STORAGE);
+        }
+    }
+
+    private void validateCameraPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission for camera is not granted");
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                mOptionsMenu.getItem(1).setEnabled(true);
+                disable = true;
+                mOpenCvCameraView.disableView();
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                Log.i(TAG, "selectedImagePath: " + selectedImagePath);
+                //loadImage(selectedImagePath);
+                myBitmap = BitmapFactory.decodeFile(selectedImagePath);
+                ImageView iv = findViewById(R.id.ivGallery);
+                iv.setImageBitmap(myBitmap);
+            }
+        }
+    }
+
+    private String getPath(Uri uri) {
+        if (uri == null) {
+            return null;
+        }
+        // prova a recuperare l'immagine prima dal Media Store
+        // questo però funziona solo per immagini selezionate dalla galleria
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection,null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
+
+    private void initClassifier() {
+        try {
+            classifier = TensorFlowImageClassifier.create(
+                    super.getAssets(), MODEL_FILE, LABEL_FILE, INPUT_WIDTH, INPUT_HEIGHT,
+                    IMAGE_MEAN, IMAGE_STD, INPUT_NAME, OUTPUT_NAME);
+        } catch (IOException e) {
+            throw new RuntimeException("classifier init problem", e);
+        }
+    }
+
+    // Questo oggetto callback è usato quando inizializzaimo la libreria OpenCV in modo asincrono
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        // Una volta che OpenCV manager è connesso viene chiamato questo metodo di
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // Una volta che OpenCV manager si è connesso con successo
+                // possiamo abilitare l'interazione con la tlc
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                     break;
+            }
+        }
+    };
+}
---
+++ app/src/main/java/it/unipr/scarpentim/pasmtftest1/tensorflow/TensorFlowImageClassifier.java
@@ -0,0 +1,206 @@
+/*
+ *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package it.unipr.scarpentim.pasmtftest1.tensorflow;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.support.v4.os.TraceCompat;
+import android.util.Log;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Vector;
+
+/**
+ * A classifier specialized to label images using TensorFlow.
+ */
+public class TensorFlowImageClassifier implements Classifier {
+
+    private static final String TAG = "ImageClassifier";
+
+    // Only return this many results with at least this confidence.
+    private static final int MAX_RESULTS = 1;
+    private static final float THRESHOLD = 0.75f;
+
+    // Config values.
+    private String inputName;
+    private String outputName;
+    private int inputSize;
+    private int outputSize;
+    private int imageMean;
+    private float imageStd;
+
+    // Pre-allocated buffers.
+    private Vector<String> labels = new Vector<String>();
+    private int[] intValues;
+    private float[] floatValues;
+    private float[] outputs;
+    private String[] outputNames;
+
+    private TensorFlowInferenceInterface inferenceInterface;
+
+    private boolean runStats = false;
+
+    private TensorFlowImageClassifier() {
+    }
+
+    /**
+     * Initializes a native TensorFlow session for classifying images.
+     *
+     * @param assetManager  The asset manager to be used to load assets.
+     * @param modelFilename The filepath of the model GraphDef protocol buffer.
+     * @param labelFilename The filepath of label file for classes.
+     * @param inputWidth    The input size. A square image of inputWidth x inputHeight is assumed.
+     * @param inputHeight   The input size. A square image of inputWidth x inputHeight is assumed.
+     * @param imageMean     The assumed mean of the image values.
+     * @param imageStd      The assumed std of the image values.
+     * @param inputName     The label of the image input node.
+     * @param outputName    The label of the output node.
+     * @throws IOException
+     */
+    public static Classifier create(
+            AssetManager assetManager,
+            String modelFilename,
+            String labelFilename,
+            int inputWidth,
+            int inputHeight,
+            int imageMean,
+            float imageStd,
+            String inputName,
+            String outputName)
+            throws IOException {
+        TensorFlowImageClassifier c = new TensorFlowImageClassifier();
+        c.inputName = inputName;
+        c.outputName = outputName;
+
+        // Read the label names into memory.
+        // TODO(andrewharp): make this handle non-assets.
+        String actualFilename = labelFilename.split("file:///android_asset/")[1];
+        Log.i(TAG, "Reading labels from: " + actualFilename);
+        BufferedReader br = null;
+        br = new BufferedReader(new InputStreamReader(assetManager.open(actualFilename)));
+        String line;
+        while ((line = br.readLine()) != null) {
+            c.labels.add(line);
+        }
+        br.close();
+
+        c.inferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
+        // The shape of the output is [N, NUM_CLASSES], where N is the batch size.
+        int numClasses =
+                (int) c.inferenceInterface.graph().operation(outputName).output(0).shape().size(1);
+        Log.i(TAG, "Read " + c.labels.size() + " labels, output layer size is " + numClasses);
+
+        // Ideally, inputWidth could have been retrieved from the shape of the input operation.  Alas,
+        // the placeholder node for input in the graphdef typically used does not specify a shape, so it
+        // must be passed in as a parameter.
+        c.inputSize = inputWidth;
+        c.outputSize = inputHeight;
+        c.imageMean = imageMean;
+        c.imageStd = imageStd;
+
+        // Pre-allocate buffers.
+        c.outputNames = new String[]{outputName};
+        c.intValues = new int[inputWidth * inputHeight];
+        c.floatValues = new float[inputWidth * inputHeight * 3];
+        c.outputs = new float[numClasses];
+
+        return c;
+    }
+
+    @Override
+    public List<Recognition> recognizeImage(final Bitmap bitmap) {
+        // Log this method so that it can be analyzed with systrace.
+        TraceCompat.beginSection("recognizeImage");
+
+        TraceCompat.beginSection("preprocessBitmap");
+        // Preprocess the image data from 0-255 int to normalized float based
+        // on the provided parameters.
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < intValues.length; ++i) {
+            final int val = intValues[i];
+            floatValues[i * 3 + 0] = (((val >> 16) & 0xFF) - imageMean) / imageStd;
+            floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - imageMean) / imageStd;
+            floatValues[i * 3 + 2] = ((val & 0xFF) - imageMean) / imageStd;
+        }
+        TraceCompat.endSection();
+
+        // Copy the input data into TensorFlow.
+        TraceCompat.beginSection("feed");
+        inferenceInterface.feed(
+                inputName, floatValues, new long[]{1, inputSize, outputSize, 3});
+        TraceCompat.endSection();
+
+        // Run the inference call.
+        TraceCompat.beginSection("run");
+        inferenceInterface.run(outputNames, runStats);
+        TraceCompat.endSection();
+
+        // Copy the output Tensor back into the output array.
+        TraceCompat.beginSection("fetch");
+        inferenceInterface.fetch(outputName, outputs);
+        TraceCompat.endSection();
+
+        // Find the best classifications.
+        PriorityQueue<Recognition> pq =
+                new PriorityQueue<Recognition>(
+                        3,
+                        new Comparator<Recognition>() {
+                            @Override
+                            public int compare(Recognition lhs, Recognition rhs) {
+                                // Intentionally reversed to put high confidence at the head of the queue.
+                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                            }
+                        });
+        for (int i = 0; i < outputs.length; ++i) {
+            if (outputs[i] > THRESHOLD) {
+                pq.add(
+                        new Recognition(
+                                "" + i, labels.size() > i ? labels.get(i) : "unknown", outputs[i], null));
+            }
+        }
+        final ArrayList<Recognition> recognitions = new ArrayList<Recognition>();
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+        TraceCompat.endSection(); // "recognizeImage"
+        return recognitions;
+    }
+
+    @Override
+    public void enableStatLogging(boolean debug) {
+        runStats = debug;
+    }
+
+    @Override
+    public String getStatString() {
+        return inferenceInterface.getStatString();
+    }
+
+    @Override
+    public void close() {
+        inferenceInterface.close();
+    }
+}
